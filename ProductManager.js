bcaa6044d921ab14b4e979334ff337fee167b87e
Lujan Benjamin <lujanbenjamin1@gmail.com>
2023-07-31 20:03:36 -0300

    correcion del ejercicio

--- ProductManager.js
+++ ProductManager.js
@@ -13,7 +13,7 @@ class ProductManager{
         if (!title || !description || !price || !thumbnail || !code || !stock) {
             return 'Error: Todos los campos son obligatorios'
           }
-          const found = this.#products.find(item => item.code === product.code)
+          const found = this.#products.find(item => item.code === code)
 
           if (found) {
             return 'Error: Producto con el mismo codigo'  
@@ -26,7 +26,7 @@ class ProductManager{
     getProducts (){
         return this.#products
     }
-    getProductsByID(){
+    getProductsByID(id){
         const product = this.#products.find((product)=>product.id ===id)
         if(!product){
             return 'producto no encontrado'
@@ -37,6 +37,20 @@ class ProductManager{
 
 const pm = new ProductManager()
 
-pm. addProduct('las aguas bajan turbias',  'largometraje', 150, 'url', 111, 25 )
+pm.addProduct('las aguas bajan turbias',  'largometraje', 150, 'url', 111, 25 )
+
+pm.addProduct('caminando por las ahi',  'documental', 170, 'url', 122, 27 )
+
+pm.addProduct('el parto de espalda',  'terror', 1000, 'url', 750, 96 )
+
+pm.addProduct('por los rincones',  'drama', 205, 'url', 72, 50 )
+
+pm.addProduct('el parto de espalda',  'terror', 1000, 'url', 111, 96 )
 
 console.log(pm.getProducts())
+
+console.log(pm.addProduct())
+
+const product = pm.getProductsByID(2)
+
+console.log(product);
